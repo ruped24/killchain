7937d769b91991374bff5b30bcf0c8c622f7bec7
Rupert Edwards <ruped24@gmail.com>
2015-11-17 14:22:47 -0500

    Update killchain.py

--- killchain.py
+++ killchain.py
@@ -22,39 +22,34 @@ Lyel = "[93m"
 
 
 def who_did_it():
-    print("        {0}".format("#" * 64))
-    print("        {0}".format("Created by: %s." % __copyright__))
-    print("        {0}".format("For training purposes only."))
-    print("        {0}".format("Version %s, License %s" %
-                              (__version__, __license__)))
-    print("        {0}".format("Written by: %s" % __author__))
-    print("        {0}".format("#" * 64 + "\n\n"))
+  print("        {0}".format("#" * 64))
+  print("        {0}".format("Created by: %s." % __copyright__))
+  print("        {0}".format("For training purposes only."))
+  print("        {0}".format("Version %s, License %s" %
+                             (__version__, __license__)))
+  print("        {0}".format("Written by: %s" % __author__))
+  print("        {0}".format("#" * 64 + "\n\n"))
 
 
-tools = {
-    1: "setoolkit",
-    2: "openvas-setup",
-    3: "veil-evasion",
-    4: "websploit"
-}
+tools = {1: "setoolkit", 2: "openvas-setup", 3: "veil-evasion", 4: "websploit"}
 
 
 def main_menu():
-    print("        {0}".format(
-        Escape + Lyel +
-        "1)  Set -- Social-Engineer Toolkit (SET), attacks against humans.\n"))
-    print("        {0}".format(
-        "2)  OpenVas --  Vulnerability scanning and vulnerability management.\n"))
-    print("        {0}".format(
-        "3)  Veil-Evasion -- Generate metasploit payloads bypass anti-virus.\n"))
-    print("        {0}".format(
-        "4)  Websploit -- WebSploit Advanced MITM Framework.\n"))
-    print("        {0}".format(Escape + Lred + "5)  Exit Kill Chain\n"))
+  print("        {0}".format(
+      Escape + Lyel +
+      "1)  Set -- Social-Engineer Toolkit (SET), attacks against humans.\n"))
+  print("        {0}".format(
+      "2)  OpenVas --  Vulnerability scanning and vulnerability management.\n"))
+  print("        {0}".format(
+      "3)  Veil-Evasion -- Generate metasploit payloads bypass anti-virus.\n"))
+  print("        {0}".format(
+      "4)  Websploit -- WebSploit Advanced MITM Framework.\n"))
+  print("        {0}".format(Escape + Lred + "5)  Exit Kill Chain\n"))
 
 
 class Header:
-        headers = {
-            1: r"""
+  headers = {
+      1: r"""
          **   ** **  **  **         ******  **                **
         /**  ** //  /** /**        **////**/**               //
         /** **   ** /** /**       **    // /**       ******   ** *******
@@ -63,13 +58,13 @@ class Header:
         /**//** /** /** /**      //**    **/**  /** **////** /** /**  /**
         /** //**/** *** ***       //****** /**  /**//********/** ***  /**
         //   // // /// ///         //////  //   //  //////// // ///   //   """,
-            2: r"""
+      2: r"""
         KK  KK iii lll lll     CCCCC  hh              iii
         KK KK      lll lll    CC    C hh        aa aa     nn nnn
         KKKK   iii lll lll    CC      hhhhhh   aa aaa iii nnn  nn
         KK KK  iii lll lll    CC    C hh   hh aa  aaa iii nn   nn
         KK  KK iii lll lll     CCCCC  hh   hh  aaa aa iii nn   nn  """,
-            3: r"""
+      3: r"""
         $$\   $$\ $$\ $$\ $$\        $$$$$$\  $$\                 $$\
         $$ | $$  |\__|$$ |$$ |      $$  __$$\ $$ |                \__|
         $$ |$$  / $$\ $$ |$$ |      $$ /  \__|$$$$$$$\   $$$$$$\  $$\ $$$$$$$\
@@ -78,49 +73,48 @@ class Header:
         $$ |\$$\  $$ |$$ |$$ |      $$ |  $$\ $$ |  $$ |$$  __$$ |$$ |$$ |  $$ |
         $$ | \$$\ $$ |$$ |$$ |      \$$$$$$  |$$ |  $$ |\$$$$$$$ |$$ |$$ |  $$ |
         \__|  \__|\__|\__|\__|       \______/ \__|  \__| \_______|\__|\__|  \__|  """,
-        }
+  }
 
 
 if __name__ == '__main__':
-    try:
-        raw_input
-    except NameError:
-        raw_input = input
-    try:
-        while True:
-            stderr.write("\x1b[2J\x1b[H")
-            call(['reset'])
-            try:
-                print(Header().headers[randint(1, 3)] + "\n\n")
-                who_did_it()
-                main_menu()
-                try:
-                    selected = int(
-                        raw_input(Escape + Lgre + gethostname() + "-gOtr00t"
-                                  ":> "))
-                    if selected < 1 or selected > 5:
-                        print("Select a number between 1 and 5")
-                        sleep(2)
-                    if selected == 5:
-                        exit(0)
-                    if selected == 1:
-                        call([tools[1]])
-                        sleep(1)
-                    if selected == 2:
-                        call([tools[2]])
-                        sleep(1)
-                    if selected == 3:
-                        call([tools[3]])
-                        sleep(1)
-                    if selected == 4:
-                        call([tools[4]])
-                        sleep(1)
-                except ValueError:
-                    print("Select a number between 1 and 5")
-                    sleep(2)
-            except SystemExit:
-                exit(0)
-    except Exception as err:
-        print("\nCheck your path, %s.\n" % err)
-        sleep(2)
-        pass
+  try:
+    raw_input
+  except NameError:
+    raw_input = input
+  try:
+    while True:
+      stderr.write("\x1b[2J\x1b[H")
+      call(['reset'])
+      try:
+        print(Header().headers[randint(1, 3)] + "\n\n")
+        who_did_it()
+        main_menu()
+        try:
+          selected = int(raw_input(Escape + Lgre + gethostname() + "-gOtr00t"
+                                   ":> "))
+          if selected < 1 or selected > 5:
+            print("Select a number between 1 and 5")
+            sleep(2)
+          if selected == 5:
+            exit(0)
+          if selected == 1:
+            call([tools[1]])
+            sleep(1)
+          if selected == 2:
+            call([tools[2]])
+            sleep(1)
+          if selected == 3:
+            call([tools[3]])
+            sleep(1)
+          if selected == 4:
+            call([tools[4]])
+            sleep(1)
+        except ValueError:
+          print("Select a number between 1 and 5")
+          sleep(2)
+      except SystemExit:
+        exit(0)
+  except Exception as err:
+    print("\nCheck your path, %s.\n" % err)
+    sleep(2)
+    pass
